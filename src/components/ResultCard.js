b4a6e5498f1e5699b701876e7df021d5d05e4c47
Daníel Freyr <83401867+foirfe@users.noreply.github.com>
2022-04-25 19:42:35 +0200

    Start of Resultspage and ResultsCard Component

--- src/components/ResultCard.js
+++ src/components/ResultCard.js
@@ -1,7 +1,11 @@
+import imagePlaceholder from "../images/img-placeholder.jpg"
+
 export default function ResultCard({place}){
     return(
-        <div className="Result">
+        <div className="ResultCard">
 <h2>{place.Name}</h2>
+<img src={place.Files[0] ? place.Files[0].Uri : imagePlaceholder} alt={place.Name} />
+<p>{place.Category.Name}</p>
         </div>
     )
 }
--- src/pages/ResultsPage.js
+++ src/pages/ResultsPage.js
@@ -1,25 +1,30 @@
 import { useEffect, useState } from "react";
+import ResultCard from "../components/ResultCard";
 
 export default function ResultsPage(){
 const [places, setPlaces] = useState([]);
+
 useEffect(()=>{
     async function getActivites(){
     const response = await fetch("https://raw.githubusercontent.com/cederdorff/react-visit-denmark/master/public/data/activities.json");
-    const data = await response.json()
-    console.log(data);
-        //const urls = ["", "https://raw.githubusercontent.com/cederdorff/react-visit-denmark/master/public/data/attractions.json"];
+    const activites = await response.json();
+    const response1 = await fetch("https://raw.githubusercontent.com/cederdorff/react-visit-denmark/master/public/data/attractions.json");
+    const attractions = await response1.json();
+    const response2 = await fetch("https://raw.githubusercontent.com/cederdorff/react-visit-denmark/master/public/data/placesToEat.json");
+    const placesToEat = await response2.json();
+    const combineddata = activites.concat(attractions, placesToEat);
+    console.log(combineddata);
+    setPlaces(combineddata);
     }
+getActivites();
+},[]);
 
-    async function getAttractions(){
-        const response = await fetch("https://raw.githubusercontent.com/cederdorff/react-visit-denmark/master/public/data/attractions.json");
-        const data = await response.json()
-        console.log(data);
-    }
-    getActivites();
-    getAttractions();
-})
     return(
+        <div className="resultspage">
         <h1>Resultater</h1>
-        
+        {places.map(place =>(
+            <ResultCard place={place} key={place.Id}/>
+        ))}
+       </div>
     );
 }
