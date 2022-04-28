6d3784812bf9b6a354cbb98514751eaee9a513ad
Ajay-Parajuli <83760685+Ajay-Parajuli@users.noreply.github.com>
2022-04-28 10:25:46 +0200

    Minor changes

--- src/pages/FilterPage.js
+++ src/pages/FilterPage.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import Form from "../components/Form";
 
 export default function FilterPage() {
@@ -12,21 +11,3 @@ export default function FilterPage() {
     </div>
   );
 }
-=======
-
-import Form from "../components/Form";
-
-
-export default function FilterPage(){
-
-        return(
-            <div className="filterpage">
-                <h1>What would you like to <span className="color">experience?</span></h1>
-                <h2>Activites</h2>
-           <Form/>
-            </div>
-        );
-    
-}
-
->>>>>>> e934e3a6cb81dc4e1381b6c93de98fcd6e121a7a
